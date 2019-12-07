e5f63bd06f5c9972aa597b1f8c9e26a3ce7baa99
Jodi N Kansagor <joedvice@gmail.com>
2019-12-06 16:46:00 -0800

    finished routes and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,11 +3,34 @@ require('../lib/utils/connect')();
 const request = require('supertest');
 const app = require('../lib/app');
 const mongoose = require('mongoose');
+const Skater = require('../lib/Models/Skater');
 
 describe('app routes', () => {
+
   // beforeAll(() => {
-  //   mongoose.connection.db.dropDatabase();
+  //   connect();
   // });
+  beforeAll(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  // afterAll(() => {
+  //   return mongoose.connection.close();
+  // });
+
+  it('has an addskaters page where one can add skaters', () => {
+    return request(app)
+      .post('/addskater')
+      .send({
+        'name': 'Jodi Bon Jodi',
+        'team': 'Willamette Kidney Thieves',
+        'skaterNumber': 908,
+        'positions': ['blocker', 'offense']
+      })
+      .then(res => {
+        expect(res.body).toEqual({ '__v': expect.any(Number), '_id': expect.any(String), 'name': 'Jodi Bon Jodi', 'positions': ['blocker', 'offense'], 'skaterNumber': 908, 'team': 'Willamette Kidney Thieves' });
+      });
+  });
 
   it('has an index page with a list of skaters', () => {
     return request(app)
@@ -17,17 +40,55 @@ describe('app routes', () => {
       });
   });
 
-  it('has an addskaters page where one can add skaters', () => {
+  it('can find one skater', async() => {
+    const skater = await Skater.create({
+      'name': 'Bonnie Thunders',
+      'team': 'Wheels of Justice',
+      'skaterNumber': 340,
+      'positions': ['jammer']
+    });
+
     return request(app)
-      .post('/addskater')
-      .send({
-        'name': 'Bonnie Thunders',
-        'team': 'Wheels of Justice',
-        'skaterNumber': 340,
-        'positions': ['jammer']
-      })
+      .get(`/${skater._id}`)
       .then(res => {
-        expect(res.body).toEqual({ '__v': expect.any(Number), '_id': expect.any(String), 'name': 'Bonnie Thunders', 'skaterNumber': 340, 'team': 'Wheels of Justice', 'positions': ['jammer'] });
+        expect(res.body).toEqual({ '__v': expect.any(Number), '_id': expect.any(String), 'name': 'Bonnie Thunders', 'positions': ['jammer'], 'skaterNumber': 340, 'team': 'Wheels of Justice' });
       });
   });
+
+  it('can update a skater', async() => {
+    const skater = await Skater.create({
+      'name': 'Tiggz',
+      'team': 'Wreckers',
+      'skaterNumber': 97,
+      'positions': ['blocker']
+    });
+
+    const updatedSkater = await Skater.findByIdAndUpdate(skater._id, {
+      'name': 'Tiggz',
+      'team': 'Jewish Roller Derby',
+      'skaterNumber': 97,
+      'positions': ['blocker']
+    });
+    return request(app)
+      .get(`/${updatedSkater._id}`)
+      .then(res => {
+        expect(res.body).toEqual({ '__v': expect.any(Number), '_id': expect.any(String), 'name': 'Tiggz', 'positions': ['blocker'], 'skaterNumber': 97, 'team': 'Jewish Roller Derby' });
+      });
+  });
+
+  it('can delete a skater', async() => {
+    const skater = await Skater.create({
+      'name': 'Tiggz',
+      'team': 'Wreckers',
+      'skaterNumber': 97,
+      'positions': ['blocker']
+    });
+
+    return request(app)
+      .get(`/${skater._id}`)
+      .then(res => {
+        expect(res.body).toEqual({ '__v': expect.any(Number), '_id': expect.any(String), 'name': 'Tiggz', 'positions': ['blocker'], 'skaterNumber': 97, 'team': 'Wreckers' });
+      });
+  });
+
 });
