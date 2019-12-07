ce65b8d94b1665f59ba8be9510b9c5b3c1c15d82
Jodi N Kansagor <joedvice@gmail.com>
2019-12-06 16:49:43 -0800

    trying to pass travis

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
-require('./lib/utils/connect')();
+// require('dotenv').config();
+// require('./lib/utils/connect')();
 
-const Skater = require('./lib/Models/Skater');
+// const Skater = require('./lib/Models/Skater');
 
 
 
-async function allMethods() {
-  const createdSkater = await Skater.create({
-    name: 'Jodi Bon Jodi',
-    team: 'Willamette Kidney Thieves',
-    skaterNumber: 908,
-    positions: ['blocker', 'offense']
-  });
+// async function allMethods() {
+//   const createdSkater = await Skater.create({
+//     name: 'Jodi Bon Jodi',
+//     team: 'Willamette Kidney Thieves',
+//     skaterNumber: 908,
+//     positions: ['blocker', 'offense']
+//   });
 
-  const oneFoundSkater = await Skater.findById(createdSkater._id);
-  const allFoundSkaters = await Skater.find();
+//   const oneFoundSkater = await Skater.findById(createdSkater._id);
+//   const allFoundSkaters = await Skater.find();
 
-  const updatedSkater = await Skater.findByIdAndUpdate(oneFoundSkater._id, { name: 'JBJ' }, { new: true });
+//   const updatedSkater = await Skater.findByIdAndUpdate(oneFoundSkater._id, { name: 'JBJ' }, { new: true });
 
-  const deletedSkater = await Skater.findByIdAndDelete(oneFoundSkater._id);
-}
+//   const deletedSkater = await Skater.findByIdAndDelete(oneFoundSkater._id);
+// }
 
